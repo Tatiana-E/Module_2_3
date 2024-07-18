38789a52f3de54c601752d5285ecc53b4df939e4
Tatiana-E <tanya.era1989@gmail.com>
2024-07-18 12:09:23 +0500

    Add files via upload

---
+++ moule_2_3.py
@@ -0,0 +1,20 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    print(my_list[index])
+    index += 1
+
+
+#numbers = [1, 2, 3, 4, 5]
+#index = 0
+#while index < len(numbers):
+ #   if numbers[index] == 3:
+  #      break
+   # print(numbers[index])
+    #index += 1
+
+
+#x = (my_list[0:len(my_list):1])
+
